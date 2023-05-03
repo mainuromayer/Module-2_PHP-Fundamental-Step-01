8cbfdec3d722985a4b3575e3837076de991eb364
mainuromayer <mainuromayer@gmail.com>
2023-05-03 11:09:42 +0600

    Leap Year | smart and readable way code writing method

--- php12.php
+++ php12.php
@@ -9,13 +9,13 @@
 # This is readable but not smart
 $year = 2000;
 if($year % 4  == 0 && $year % 100 == 0 && $year % 400 == 0){
-    echo "{$year} is a leap year";
+    echo "{$year} is a leap year.";
 }elseif($year % 4  == 0 && $year % 100 == 0) {
-    echo "{$year} is not a leap year";
+    echo "{$year} is not a leap year.";
 }elseif($year % 4  == 0) {
-    echo "{$year} is a leap year";
+    echo "{$year} is a leap year.";
 }else{
-    echo "{$year} is not a leap year";
+    echo "{$year} is not a leap year.";
 }
 
 echo "\n";
